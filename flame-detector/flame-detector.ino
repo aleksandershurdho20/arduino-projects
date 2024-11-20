799f94b7268ebf127e0fd90131b8dcb0300ddc98
aleksandershurdho20 <aleksandershurdho@gmail.com>
2024-11-21 00:00:31 +0100

    feat: added led to the fire sensor

--- flame-detector/flame-detector.ino
+++ flame-detector/flame-detector.ino
@@ -1,28 +1,34 @@
 
 const int flameSensor = A0;
 const int buzzer = 8;
+const int led = 12;
 int flameVal;
 void setup() {
   Serial.begin(9600);
   pinMode(buzzer,OUTPUT); // define the buzzer as output
   pinMode(flameSensor,INPUT);// define the flame sensor as input
   // put your setup code here, to run once:
+  pinMode(led,OUTPUT);// define the flame led as output
+
 }
 
 void loop() {
   int flameVal = analogRead(flameSensor);
 
   if(flameVal < 1000 ) {
-    // fire the alarm 
+    // fire the alarm and turn led on
     delay(200);
     tone(buzzer,600,800);
     delay(200);
+    digitalWrite(led,HIGH);
 
   }
   else{
     // stop alarm
     digitalWrite(buzzer,LOW);
     noTone(buzzer);
+    digitalWrite(led,LOW);
+
   }
   Serial.println(flameVal);
 }
